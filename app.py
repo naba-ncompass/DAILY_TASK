8381009e143f04d480f1e6a6942ab87d52e183ba
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-21 13:06:24 +0530

    blueprint implemented

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
 from flask import Flask,jsonify,request
+from route import bp
 import json
 import db
 
 app = Flask(__name__)
 
-@app.route("/")
-def read_operation():
-    var = db.read_from_student()
-    json_output = json.dumps(var)
-    return jsonify(var)
-
-@app.route("/insert", methods=['POST'])
-def insert_operation():
-    input_data = {}
-    input_data = request.get_json()
-    response = db.insert_into_student(input_data)
-    return response
-
-@app.route("/update", methods=['PUT'])
-def update_operation():
-    input_data = {}
-    input_data = request.get_json()
-    response = db.update_student(input_data)
-    return response
-
-@app.route("/delete", methods=['DELETE'])
-def delete_operation():
-    input_data = {}
-    input_data = request.get_json()
-    response = db.delete_from_student(input_data)
-    return response
+app.register_blueprint(bp)
 
 if __name__ == '__main__':
     app.env = 'development'
