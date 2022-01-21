325eb37d9e0e238e60671c6de7648e65c19168cb
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-21 22:23:06 +0530

    error handler file for error management

---
+++ utilities/error_handler.py
@@ -0,0 +1,34 @@
+from flask import Blueprint,jsonify
+from werkzeug.exceptions import HTTPException
+import json
+
+bp_errors = Blueprint('bp_errors', __name__)
+
+@bp_errors.app_errorhandler(Exception)
+def handle_exception(e):
+    if isinstance(e, HTTPException):
+        response = {
+            "success":False,
+            "code": e.code,
+            "name": e.name,
+            "data": [],
+            "message": e.description
+        }
+        return jsonify(response)
+    else:
+        response = {
+            "success":False,
+            "data": [],
+            "message": "something went wrong!!!"
+        }
+        return jsonify(response)
+
+
+def generate_error_response(e):
+
+    response = {
+        "success":False,
+        "data":[],
+        "message": e.msg
+    }
+    return jsonify(response)
