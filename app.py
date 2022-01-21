500d6e4b77f29b4fb1c19896eed856335171dde8
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-22 00:02:41 +0530

    comments removed

--- app.py
+++ app.py
@@ -9,18 +9,6 @@ app = Flask(__name__)
 app.register_blueprint(route.bp_routes)
 app.register_blueprint(error_handler.bp_errors)
 
-# @app.errorhandler(HTTPException)
-# def handle_exception(e):
-#     response = e.get_response()
-
-#     response.data = json.dumps({
-#         "code": e.code,
-#         "name": e.name,
-#         "description": e.description,
-#     })
-#     response.content_type = "application/json"
-#     return response
-
 
 if __name__ == '__main__':
     app.env = 'development'
