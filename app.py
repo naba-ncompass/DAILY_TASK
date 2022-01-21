39993c85d8e4b2bb12d4b20a283ea61ad24dfaa1
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-21 21:55:56 +0530

    error handler blueprint added

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask,jsonify,request
-from route import bp
+from Student import route
 import json
-import db
+from Utilities import db,error_handler
+from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 
-app.register_blueprint(bp)
+app.register_blueprint(route.bp_routes)
+app.register_blueprint(error_handler.bp_errors)
+
+# @app.errorhandler(HTTPException)
+# def handle_exception(e):
+#     response = e.get_response()
+
+#     response.data = json.dumps({
+#         "code": e.code,
+#         "name": e.name,
+#         "description": e.description,
+#     })
+#     response.content_type = "application/json"
+#     return response
+
 
 if __name__ == '__main__':
     app.env = 'development'
+    app.debug = True
     app.run()
