588044d7946d9d156f12f475b2d5218f60d48b60
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-24 18:49:14 +0530

    routes names changed

--- student/route.py
+++ student/route.py
@@ -1,14 +1,14 @@
-from flask import Blueprint,jsonify
-from Utilities import db
-import json
+from flask import Blueprint
 from Student import controller
 
-bp_routes = Blueprint('bp_routes', __name__)
+student_routes = Blueprint('student_routes', __name__)
 
-bp_routes.route('/')(controller.read_operation)
+student_routes.route('/')(controller.read_operation)
 
-bp_routes.route("/insert", methods=['POST'])(controller.insert_operation)
+student_routes.route('/read')(controller.read_where_operation)
 
-bp_routes.route("/update", methods=['PUT'])(controller.update_operation)
+student_routes.route("/insert", methods=['POST'])(controller.insert_operation)
 
-bp_routes.route("/delete", methods=['DELETE'])(controller.delete_operation)
+student_routes.route("/update", methods=['PUT'])(controller.update_operation)
+
+student_routes.route("/delete", methods=['DELETE'])(controller.delete_operation)
