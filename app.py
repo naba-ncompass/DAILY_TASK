8fbf5c94e8f644084a956ac1006cf8e1a7ee4f59
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-24 18:47:26 +0530

    blueprints for device use case added

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask,jsonify,request
-from Student import route
-import json
-from Utilities import db,error_handler
-from werkzeug.exceptions import HTTPException
+from flask import Flask
+from Student import route as student_route
+from Utilities import error_handler
+from Device import route as device_route
 
 app = Flask(__name__)
 
-app.register_blueprint(route.bp_routes)
+app.register_blueprint(student_route.student_routes)
 app.register_blueprint(error_handler.bp_errors)
-
+app.register_blueprint(device_route.device_routes)
 
 if __name__ == '__main__':
     app.env = 'development'
