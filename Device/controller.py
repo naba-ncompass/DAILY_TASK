6213e498842cc84bc86a8b27efcdf884102e498a
Ashish Dev Nagar <46266088+ashishdevnagar@users.noreply.github.com>
2022-01-25 12:36:11 +0530

    compressed the read operation

--- Device/controller.py
+++ Device/controller.py
@@ -1,19 +1,28 @@
 from flask import jsonify,request
 from datetime import datetime
-from Utilities import db,error_handler
+from Utilities import db,error_handler,compression
 from Device import validation
 
 
 def read_operation():
     start_time = datetime.now()
-    query = "select * from uc3"
-    output = db.read_from_table(query)
-    return jsonify(give_response(data=output, message='operation successful', start_time=start_time))
+    params = request.args
+    validity = validation.validate_device(params)
+    if isinstance(validity, bool):
+        query = f"select * from uc3 where device = '{params['device']}'"
+        output = db.read_from_table(query)
+        
+        response = give_response(data=output, message='operation successful', start_time=start_time)
+        compressed_response = compression.make_compress(response)
+        return compressed_response
+    
+    else:
+        return jsonify(error_handler.generate_error_response(validity))
 
 
 def peak_between_times():
-    params = request.get_json()
     start_time = datetime.now()
+    params = request.get_json()
     validity = validation.validate_device(params)
     if isinstance(validity, bool):
         query = f"select max(CONSUMPTION), DEVICE FROM uc3 where TIME(TIME) > '{params['time1']}' AND TIME(TIME) < '{params['time2']}' group by DEVICE having DEVICE = '{params['device']}'"
